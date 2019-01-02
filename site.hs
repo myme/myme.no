5bddfcaa526dc1f22c7dbec933f8c070bd3fd841
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-01-02 20:03:32 +0100

    Formatting

--- site.hs
+++ site.hs
@@ -1,42 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Hakyll
+import Hakyll
 
 main :: IO ()
 main = hakyllWith config $ do
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route   idRoute
+      compile copyFileCompiler
 
     match "js/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route   idRoute
+      compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+      route   idRoute
+      compile compressCssCompiler
 
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+      route $ setExtension "html"
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html"    postCtx
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
 
     create ["posts.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let postsCtx =
-                    constField "title" "Posts" <>
-                    listField "posts" postCtx (return posts) <>
-                    defaultContext
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "posts/*"
+        let postsCtx =
+              constField "title" "Posts" <>
+              listField "posts" postCtx (return posts) <>
+              defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/posts.html" postsCtx
-                >>= loadAndApplyTemplate "templates/default.html" postsCtx
-                >>= relativizeUrls
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/posts.html" postsCtx
+          >>= loadAndApplyTemplate "templates/default.html" postsCtx
+          >>= relativizeUrls
 
     create ["feed.xml"] $ do
       route idRoute
@@ -48,30 +48,28 @@ main = hakyllWith config $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            lastPost:earlierPosts <- take 5 <$> (recentFirst =<< loadAllSnapshots "posts/*" "content")
-            let indexCtx =
-                    constField "title" "Home" <>
-                    listField "lastPost" postCtx (return [lastPost]) <>
-                    listField "posts" postCtx (return earlierPosts) <>
-                    defaultContext
+          lastPost:earlierPosts <- take 5 <$> (recentFirst =<< loadAllSnapshots "posts/*" "content")
+          let indexCtx =
+                constField "title" "Home" <>
+                listField "lastPost" postCtx (return [lastPost]) <>
+                listField "posts" postCtx (return earlierPosts) <>
+                defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+          getResourceBody
+            >>= applyAsTemplate indexCtx
+            >>= loadAndApplyTemplate "templates/default.html" indexCtx
+            >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
 config :: Configuration
 config = defaultConfiguration
-       { destinationDirectory = "public"
-       , deployCommand = "rsync -Pavz --delete ./public deque:/data/myme.no"
-       }
+  { destinationDirectory = "public"
+  , deployCommand = "rsync -Pavz --delete ./public deque:/data/myme.no"
+  }
 
 postCtx :: Context String
-postCtx =
-    dateField "date" "%F" <>
-    defaultContext
+postCtx = dateField "date" "%F" <> defaultContext
 
 feedConfig :: FeedConfiguration
 feedConfig = FeedConfiguration
